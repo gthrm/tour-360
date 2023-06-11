18f4a19d73747df6cbc4f168e02327bd727d10e4
Roman Meshcheriakov <rmeshcheriakov@apple.com>
2023-06-11 22:21:45 +0200

    feat: zoom added

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+function addIcon(container) {
+  const icon = document.createElement("i");
+  icon.className = "fas fa-arrows-alt panorama-icon"; // Added your CSS class
+  container.appendChild(icon);
+}
+
+function addZoomButtons(container, camera) {
+  const zoomInButton = document.createElement("i");
+  zoomInButton.className = "fas fa-plus zoom-button";
+  container.appendChild(zoomInButton);
+
+  const zoomOutButton = document.createElement("i");
+  zoomOutButton.className = "fas fa-minus zoom-button";
+  container.appendChild(zoomOutButton);
+
+  let zoomLevel = 0;
+
+  zoomInButton.addEventListener("click", function (event) {
+    event.stopPropagation();
+    if (zoomLevel < 3) {
+      zoomLevel++;
+      camera.zoom += 0.5;
+      camera.updateProjectionMatrix();
+    }
+  });
+
+  zoomOutButton.addEventListener("click", function (event) {
+    event.stopPropagation();
+    if (zoomLevel > -1) {
+      zoomLevel--;
+      camera.zoom -= 0.5;
+      camera.updateProjectionMatrix();
+    }
+  });
+}
+
 const panoramas = ["./1.png", "./2.png", "./3.png"];
 const body = document.querySelector("body");
 for (let i = 0; i < panoramas.length; i++) {
   const container = document.createElement("div");
   container.className = "panorama";
+  addIcon(container);
   body.appendChild(container);
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(container.clientWidth, container.clientHeight);
@@ -17,7 +55,7 @@ for (let i = 0; i < panoramas.length; i++) {
     1000
   );
   camera.position.z = 0.01;
-
+  addZoomButtons(container, camera);
   const controls = new OrbitControls(camera, renderer.domElement);
 
   const scene = new THREE.Scene();
